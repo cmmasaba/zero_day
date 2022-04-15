0fe00fce85b7cd9b0440eaaba5a168b9d4413450
Collins Mmasaba <85477468+cmmasaba@users.noreply.github.com>
2022-04-15 22:27:40 +0300

    Update files.sh

--- 0x00-vagrant/files.sh
+++ 0x00-vagrant/files.sh
@@ -4,7 +4,7 @@ if read -t 15 -p "Enter the file name: " name
 then
 	read -p "Enter the file permissions in absolute mode: " perm
 	touch $name
-	chmod $perm
+	chmod $perm $name
 	vim $name
 else
 	echo "No name provided, try again"
